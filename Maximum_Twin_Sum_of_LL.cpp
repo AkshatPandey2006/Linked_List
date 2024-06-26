9cf436e7c0788581d099b43fb5133a6c3050327d
Akshat Pandey <128710323+AkshatPandey2006@users.noreply.github.com>
2024-06-26 15:53:08 +0530

    Create Maximum_Twin_Sum_of_LL.cpp

---
+++ Maximum_Twin_Sum_of_LL.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    ListNode* inverse(ListNode* head){
+        ListNode* temp;
+        ListNode* curr=head;
+        ListNode* prev=NULL;
+
+        while(curr){
+            temp=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        return prev;
+    }
+
+    int pairSum(ListNode* head) {
+        if(head==NULL) return 0;
+
+        if(head->next->next==NULL) return head->val + head->next->val;
+
+        ListNode* fast= head;
+        ListNode* slow=head;
+        while(fast){
+            fast=fast->next->next;
+            slow=slow->next;
+        }
+       
+        ListNode* head1 = inverse(slow);
+
+        int x=0;
+        while(head && head1){
+            x=max(x,head1->val + head->val);
+            head=head->next;
+            head1=head1->next;
+        }
+
+        return x;
+    }
+};
